590e697f6216f509615012d8b2fb36e08e5b8987
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2023-10-16 23:26:48 +0200

    calculadora trucha medio hecha

--- tema2/05_EjercicioCalculadora/src/app/app.module.ts
+++ tema2/05_EjercicioCalculadora/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,7 +15,8 @@ import { CalculadoraComponent } from './components/calculadora/calculadora.compo
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tema2/05_EjercicioCalculadora/src/app/components/calculadora/calculadora.component.ts
+++ tema2/05_EjercicioCalculadora/src/app/components/calculadora/calculadora.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  result: string = '';
+  currentNumber: string = '';
+  operator: string = '';
+  digits: string[] = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
 
+  addToResult(digit: string) {
+    this.currentNumber += digit;
+    this.result = this.currentNumber;
+  }
+
+  addOperator(operator: string) {
+    if (this.currentNumber !== '') {
+      this.operator = operator;
+      this.result = this.currentNumber + ' ' + operator;
+      this.currentNumber = '';
+    }
+  }
+
+  clearResult() {
+    this.result = '';
+    this.currentNumber = '';
+    this.operator = '';
+  }
+
+  calculate() {
+    if (this.currentNumber !== '' && this.operator !== '') {
+      const num1 = parseFloat(this.currentNumber);
+      const num2 = parseFloat(this.result.split(' ')[0]);
+      switch (this.operator) {
+        case '+':
+          this.result = (num2 + num1).toString();
+          break;
+        case '-':
+          this.result = (num2 - num1).toString();
+          break;
+        case '*':
+          this.result = (num2 * num1).toString();
+          break;
+        case '/':
+          this.result = (num2 / num1).toString();
+          break;
+      }
+      this.operator = '';
+      this.currentNumber = '';
+    }
+  }
 }
